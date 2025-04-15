69a37b07995e3e5810158128180cbd027913d364
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-15 14:03:43 +0900

    feat: MainColor 추가, MainView 임시 추가

---
+++ ThankIt/ThankIt/Feature/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  ThankIt
+//
+//  Created by 김민석 on 4/15/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
