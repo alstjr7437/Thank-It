341626ebf7079203e5888afefa521b2f7a704975
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-15 17:36:48 +0900

    feat: 포스트잇 컬러를 Enum으로 생성

--- ThankIt/ThankIt/Data/Thank.swift
+++ ThankIt/ThankIt/Data/Thank.swift
@@ -17,8 +17,14 @@ struct Thank {
 }
 
 enum PostIt {
-    case square(color: String)
+    case square(color: PostItColor)
     case clova
     case apple
 }
 
+enum PostItColor: String {
+    case yellow = "MainColor"
+    case pink = "PostColor1"
+    case blue = "PostColor2"
+    case purple = "PostColor3"
+}
