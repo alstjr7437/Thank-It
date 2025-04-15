74dd8fb62e7c233d32c4284938fd9c5c57c3841a
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-15 13:50:42 +0900

    chore: 프로젝트 생성

---
+++ ThankIt/ThankIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThankIt
+//
+//  Created by 김민석 on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThankIt/ThankIt/ThankItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThankItApp.swift
+//  ThankIt
+//
+//  Created by 김민석 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ThankItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
