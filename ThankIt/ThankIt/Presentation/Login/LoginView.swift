4d58eef1058a29e17fbc2d25ae4c0a5daadf883c
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-15 14:26:30 +0900

    프로젝트 기본 셋팅(#1) (#2)
    * chore: Issue, PR 템플릿 추가
    
    * chore: ignore, CI 파일 추가
    
    * chore: 프로젝트 생성
    
    * chore: 프로젝트 폴더링 기본 세팅
    
    * feat: MainColor 추가, MainView 임시 추가
    
    * chore: 폴더 이름 변경
    
    * chore: 기본 폴더링 완료

---
+++ ThankIt/ThankIt/Presentation/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  ThankIt
+//
+//  Created by 김민석 on 4/15/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
