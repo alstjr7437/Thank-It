fc42513dcdb5f626e8bc3e95fc1ec851ea0b2620
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-17 19:55:07 +0900

    feat: FireBase Core 설정

--- ThankIt/ThankIt/Application/ThankItApp.swift
+++ ThankIt/ThankIt/Application/ThankItApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct ThankItApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainView(thanks: DummyData.Thanks)
