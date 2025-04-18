d667ee1f90a1a7f243ab2e647a9636522eef79bd
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-18 12:37:47 +0900

    refactor: 파이어베이스 매니저 Error 처리 추가

--- ThankIt/ThankIt/Persistent/FirebaseManager.swift
+++ ThankIt/ThankIt/Persistent/FirebaseManager.swift
@@ -14,10 +14,14 @@ final class FirebaseManager {
     
     let db = Firestore.firestore()
     
-    func create(_ data: EntityRepresentable) async throws {
-        guard let dataDictionary = data.asDictionary else { return }
-        try await db.collection(data.entityName.rawValue).document(data.documentID).setData(dataDictionary)
-        print("✅ Thank added to Firestore")
+    func create<T: EntityRepresentable>(_ data: T) async throws -> T {
+        guard let dataDictionary = data.asDictionary else { throw Error.encodingFailed }
+        try await db
+            .collection(data.entityName.rawValue)
+            .document(data.documentID)
+            .setData(dataDictionary)
+        
+        return data
     }
     
     func fetch<T: Decodable>(
@@ -26,11 +30,43 @@ final class FirebaseManager {
     ) async throws -> [T] {
         let snapshot = try await db.collection(collectionType.rawValue).getDocuments()
         
-        let items: [T] = try snapshot.documents.compactMap { doc in
-            let data = try JSONSerialization.data(withJSONObject: doc.data())
-            return try JSONDecoder().decode(T.self, from: data)
+        let items: [T] = try snapshot.documents.compactMap { document in
+            guard let jsonData = try? JSONSerialization.data(withJSONObject: document.data()),
+                  let decoded = try? JSONDecoder().decode(T.self, from: jsonData)
+            else {
+                throw Error.decodingFailed
+            }
+            
+            return decoded
         }
         
         return items
+    }
+extension FirebaseManager {
+    enum Error: LocalizedError {
+        case addFailed(underlying: Swift.Error)
+        case fetchFailed(underlying: Swift.Error)
+        case deleteFailed(underlying: Swift.Error)
+        case updateFailed(underlying: Swift.Error)
+        
+        case decodingFailed
+        case encodingFailed
+        
+        var errorDescription: String? {
+            switch self {
+            case .addFailed(let error):
+                "데이터를 추가하는데 실패했습니다: \(error)"
+            case .fetchFailed(let error):
+                "데이터를 읽어오는데 실패했습니다: \(error)"
+            case .deleteFailed(let error):
+                "데이터를 삭제하는데 실패했습니다: \(error)"
+            case .updateFailed(let error):
+                "데이터를 업데이트하는데 실패했습니다: \(error)"
+            case .encodingFailed:
+                "데이터를 encoding하는데 실패했습니다"
+            case .decodingFailed:
+                "데이터를 decoding하는데 실패했습니다"
+            }
+        }
     }
 }
