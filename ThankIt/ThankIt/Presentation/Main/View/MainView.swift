0f241deb9b8784b25422ff27312a7ba829abdfac
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-24 14:27:07 +0900

    refactor: popupView 삭제

--- ThankIt/ThankIt/Presentation/Main/View/MainView.swift
+++ ThankIt/ThankIt/Presentation/Main/View/MainView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import PopupView
 
 struct MainView: View {
     @StateObject private var container = MainContainer()
--- ThankIt/ThankIt/Presentation/ThankCreate/View/ThankCreateView.swift
+++ ThankIt/ThankIt/Presentation/ThankCreate/View/ThankCreateView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import PopupView
 
 struct ThankCreateView: View {
     
@@ -23,37 +22,49 @@ struct ThankCreateView: View {
     }
 
     var body: some View {
-        VStack {
-            ScrollView {
-                VStack(spacing: 10) {
-                    ZStack(alignment: .topLeading) {
-                        PostItBackgroundView(postIt: form.selectedPostIt)
-                            .shadow(radius: 4, x: 1, y: 4)
-
-                        TextEditor(text: $form.content)
-                            .padding(20)
-                            .background(Color.clear)
-                            .scrollContentBackground(.hidden)
-                            .foregroundColor(.black)
-                            .font(.basicFont)
+        ZStack {
+            VStack {
+                ScrollView {
+                    VStack(spacing: 10) {
+                        ZStack(alignment: .topLeading) {
+                            PostItBackgroundView(postIt: form.selectedPostIt)
+                                .shadow(radius: 4, x: 1, y: 4)
+                            
+                            TextEditor(text: $form.content)
+                                .padding(20)
+                                .background(Color.clear)
+                                .scrollContentBackground(.hidden)
+                                .foregroundColor(.black)
+                                .font(.basicFont)
+                        }
+                        .frame(width: 350, height: 350)
+                        .padding(.bottom, 20)
+                        
+                        CreateSelectView(title: "공개 여부", isOn: $form.isPublic)
+                        CreateSelectView(title: "익명 여부", isOn: $form.isAnonymous)
+                        DatePickerView(title: "날짜 선택", selectedDate: $form.selectedDate)
+                        PostItPickerView(title: "포스트잇 선택", selectedPostIt: $form.selectedPostIt)
                     }
-                    .frame(width: 350, height: 350)
-                    .padding(.bottom, 20)
-
-                    CreateSelectView(title: "공개 여부", isOn: $form.isPublic)
-                    CreateSelectView(title: "익명 여부", isOn: $form.isAnonymous)
-                    DatePickerView(title: "날짜 선택", selectedDate: $form.selectedDate)
-                    PostItPickerView(title: "포스트잇 선택", selectedPostIt: $form.selectedPostIt)
+                    .padding()
                 }
-                .padding()
+                Spacer()
+                CreateButtonView(text: thank == nil ? "생성하기" :"수정하기", isDisabled: isButtonDisabled) {
+                    if let thank = thank {
+                        container.send(.updateThank(form: form, id: thank.id.uuidString))
+                    } else {
+                        container.send(.createThank(form: form))
+                    }
+                }
             }
-            Spacer()
-            CreateButtonView(text: thank == nil ? "생성하기" :"수정하기", isDisabled: isButtonDisabled) {
-                if let thank = thank {
-                    container.send(.updateThank(form: form, id: thank.id.uuidString))
-                } else {
-                    container.send(.createThank(form: form))
+            if state.isLoading {
+                VStack(spacing: 16) {
+                    ProgressView(thank == nil ? "생성 중..." : "수정 중...")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .foregroundColor(.white)
+                        .tint(.white)
                 }
+                .padding(24)
+                .background(RoundedRectangle(cornerRadius: 12).fill(Color.black.opacity(0.8)))
             }
         }
         
@@ -74,23 +85,6 @@ struct ThankCreateView: View {
                 onComplete()
                 dismiss()
             }
-        }
-        
-        // MARK: Loading
-        .popup(
-            isPresented: Binding(
-                get: { state.isLoading },
-                set: { _ in } // 사용자가 직접 닫지 못하도록 설정
-            )
-        ) {
-            VStack(spacing: 16) {
-                ProgressView(thank == nil ? "생성 중..." : "수정 중...")
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .foregroundColor(.white)
-                    .tint(.white)
-            }
-            .padding(24)
-            .background(RoundedRectangle(cornerRadius: 12).fill(Color.black.opacity(0.8)))
         }
         
         .onAppear {
