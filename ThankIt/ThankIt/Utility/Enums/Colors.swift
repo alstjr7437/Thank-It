08dfbff41a9309e6f7e30ed8de3daeeb952b94e1
김민석|Minseok Kim <alstjr7437@naver.com>
2025-06-21 15:47:36 +0900

    refactor: extension 및 Enum 파일을 Utility 파일로 이동

---
+++ ThankIt/ThankIt/Utility/Enums/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  ThankIt
+//
+//  Created by 김민석 on 6/21/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let loadingBackground = Color.black.opacity(0.8)
+}
---
+++ ThankIt/ThankIt/Utility/Enums/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  ThankIt
+//
+//  Created by 김민석 on 6/21/25.
+//
+
+import Foundation
+
